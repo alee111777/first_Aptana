f571b74fe4998d1470a2a05d226a6f4708363fba
Anthony Lee <alee111777@gmail.com>
2013-10-06 16:48:14 -0700

    testing git in aptana

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ FirstAptana::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
+  ###adding this to test git one more time
 end
